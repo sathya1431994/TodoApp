70bf893f30c83bfc17fa7e91946983646232fbd1
sathya moorthy R <sathyamoorthy.ravichandran@kggroup.com>
2017-09-04 18:53:05 +0530

    Update TodoController.java

--- src/main/java/com/example/todo/TodoController.java
+++ src/main/java/com/example/todo/TodoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://todo4yu.herokuapp.com", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/todolist")
 public class TodoController {
 	@Autowired
